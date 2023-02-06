b4d5245b7567920a653b3eaedcb647adf67344f8
amir <a.moharami@gmail.com>
2023-02-06 14:20:38 +0330

    add authcontroller

---
+++ views/login.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label class="form-label">login</label>
+                    <input type="text" class="form-control" name="subject">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">email</label>
+                    <input name="email" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">body</label>
+                    <input name="body" type="text" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+    </div>
+</div>
---
+++ views/register.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label class="form-label">First Name</label>
+                    <input type="text" class="form-control" name="firstname">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Last Name</label>
+                    <input name="lastname" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Email :</label>
+                    <input name="email" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Password :</label>
+                    <input name="password" type="password" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Password Repeat:</label>
+                    <input name="confirm" type="password" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">body</label>
+                    <input name="body" type="text" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+    </div>
+</div>
